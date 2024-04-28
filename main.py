30d0c1d5d6997ec02942f10ea30970264624f77c
Leandrosaints <leo320487@gmail.com>
2024-04-28 02:11:40 -0400

    subindo atualizaçoes de rotina

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import streamlit as st
 # Definir o layout da página
 st.set_page_config(layout="wide", initial_sidebar_state="collapsed")
 
-# Título da página
-st.title("Sala de Aula")
 
 # Lista de itens da sala de aula
 classroom_items = [
@@ -14,29 +12,16 @@ classroom_items = [
     "Iluminação adequada",
     "Quadro branco disponível"
 ]
-
+Ambiente = 'laboratorio'
 # Criar o card com bordas arredondadas
-st.markdown("""
-<div style="background-color: white; border-radius: 10px; padding: 20px;">
-    <h2 style="color: #333;">Itens da Sala de Aula</h2>
-    <ul style="color: #333; font-size: 1.2rem;">
+
+st.markdown(f"""
+<div style="border-radius: 10px; padding: 20px;">
+    <h2 style="color: #333;">Check List de itens em {Ambiente} </h2>
+    
 """, unsafe_allow_html=True)
 
 # Exibir a lista de itens dentro do card
 for item in classroom_items:
-    st.markdown(f"    <li style='margin-bottom: 10px;'>✅ {item}</li>", unsafe_allow_html=True)
+    st.markdown(f"✅ {item}</div>", unsafe_allow_html=True)
 
-st.markdown("""
-    </ul>
-</div>
-""", unsafe_allow_html=True)
-
-# Ajustar o estilo da página
-st.markdown("""
-<style>
-    .stApp {
-        background-color: #f0f0f0;
-        padding: 2rem;
-    }
-</style>
-""", unsafe_allow_html=True)
