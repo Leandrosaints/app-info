a7fb26c720ce90f7e4f054991ea1bc79a33f7bce
Leandrosaints <leo320487@gmail.com>
2024-05-04 14:23:18 -0400

    CHORE:melhoria no layout e design.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import base64
 import streamlit as st
-from src.styles_css import custom_css, custom_main, custom_menu
-from streamlit_option_menu import option_menu
+from src.styles_css import custom_css, custom_main, hidden_menu
+
 def add_bg_from_local(image_file):
     with open(image_file, "rb") as image_file:
         encoded_string = base64.b64encode(image_file.read())
@@ -14,10 +14,25 @@ def add_bg_from_local(image_file):
         background-position: center bottom;
         background-repeat: no-repeat;
         position: relative;
-         margin-top: 0px; /* Reduz a margem superior */
-       
+        margin-top: 0px; /* Reduz a margem superior */
     }}
-    
+    </style>
+    """,
+        unsafe_allow_html=True
+    )
+
+def add_img_app(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+        f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
+        background-size: cover;
+        background-position: center bottom;
+        background-repeat: no-repeat;
+    }}
     </style>
     """,
         unsafe_allow_html=True
@@ -32,34 +47,47 @@ classroom_items = [
     "Ar condicionado funcionando",
     "Computador funcionando",
     "Iluminação adequada",
-    "Quadro branco disponível"
+    "Quadro branco disponível",
 ]
+
+links = [
+    {"text": "home", "url": "https://example.com/1", "icon": "fa-sharp fa-solid fa-house"},
+    {"text": "form", "url": "https://example.com/2","icon": "fa-solid fa-list-check"},
+    {"text": "contact", "url": "https://example.com/3","icon": "fa-solid fa-mobile" }
+]
+
 
 Ambiente = 'laboratorio'
 
+def generate_links(links):
+    links_html = "<div class='st-emotion-cache-18ni7ap'>"
+    links_html += """<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">"""
+    links_html += "<div class='link-container'>"
+    for link in links:
+        links_html += "<div class='link-item'>"
+        if "icon" in link:
+            links_html += f"<a href='{link['url']}'><i class='{link['icon']}'></i></a>"
+        links_html += f"<a href='{link['url']}'>{link['text']}</a>"
+        links_html += "</div>"
+    links_html += "</div>"
+    links_html += "</div>"
+    return st.markdown(links_html, unsafe_allow_html=True)
+
+
+# Adicione os links à página usando st.markdown
+generate_links(links)
+
 # Carregar o arquivo CSS personalizado
+st.markdown(hidden_menu, unsafe_allow_html=True)
 st.markdown(custom_css, unsafe_allow_html=True)
-
-
 
 # Usar o estilo personalizado dentro de um contêiner
 with st.container() as container:
-
-
-
-    selected = option_menu(
-        menu_title=None,
-        options=["Home", "Upload", "Analytics", 'Settings', 'Contact'],
-        icons=['house', 'cloud-upload', "graph-up-arrow", 'gear', 'phone'],
-        menu_icon="cast",
-        orientation='horizontal',
-        styles=custom_menu
-    )
-
     st.markdown('<div class="custom-container"> '
-                f'<h1>Check List de itens em {Ambiente}</h1>',unsafe_allow_html=True)
-    add_bg_from_local('src/senai-web.jpg')
+                f'<h1>Check List de itens em {Ambiente}</h1>', unsafe_allow_html=True)
 
+    add_bg_from_local('src/senai-web.jpg')
+    #add_img_app('src/img.jpg')
 
     custom = "<div class='container-main'>"
     for item in classroom_items:
@@ -67,5 +95,5 @@ with st.container() as container:
     custom += "</div>"
     st.markdown(custom, unsafe_allow_html=True)
 
+# Carregar o estilo CSS personalizado para a parte principal da página
 st.markdown(custom_main, unsafe_allow_html=True)
-
--- src/styles_css.py
+++ src/styles_css.py
@@ -1,5 +1,14 @@
 
+cores = """
 
+
+    .color1 { #0444b4 };
+    .color2 { #9ec4f1 };
+    .color3 { #0444ac };
+    .color4 { #033895 };
+    .color5 { #b4c4dc };
+        
+"""
 custom_css = """
     <style>
 
@@ -15,6 +24,8 @@ custom_css = """
         .custom-container h1 {
             text-align:center;
         }
+     
+        
 
     </style>
 """
@@ -29,7 +40,7 @@ custom_main = """
             transform: translate(-50%, -50%);
             padding: 50px;
             width:360px;
-            height:300px;
+            height:auto;
             background-color: rgba(255, 255, 255, 0.8);
             border-radius: 10px;
             box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
@@ -38,26 +49,61 @@ custom_main = """
             font-size:18px;
             margin-left:2px;   
     }
-
+        
+     
+      
 
     </style>
 """
 
-custom_menu = {
 
-    "container": {
-        "height": "100px",
-        "padding": "0!important",
-        "background-color": "#fafafa"
-    },
 
-    "icon": {"color": "orange", "font-size": "25px"},
-    "nav-link": {
-        "font-size": "16px",
-        "text-align": "center",
-        "margin": "0px",
-        "--hover-color": "#eee"
-    },
-    "nav-link-selected": {"background-color": "green"}
-}
 # Aplicar os estilos CSS personalizados
+
+hidden_menu= """
+    
+    <style>
+          
+        .st-emotion-cache-18ni7ap {
+            padding-top:20px;
+            height:80px;
+            background:#065dac;
+            display: flex;
+            justify-content: space-evenly;
+
+            
+        }
+       
+        .link-container {
+            display: flex;
+            gap:100px;
+            
+        }
+        .link-item {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            text-decoration: none;
+            justify-content: space-evenly;
+        }
+        .link-item i {
+            font-size: 24px;
+            margin-bottom: 5px;
+            color:white;
+        }
+        .link-item a {
+            text-decoration: none;
+            color: inherit;
+        }
+            
+            
+        
+        
+        #MainMenu {
+            visibility: hidden;
+            
+        }
+        footer {visibility: hidden;}
+        
+    </style> 
+"""
