964f9dc65c2e1f8b7d48352987eb760b00e79851
Leandrosaints <l210598l>
2024-05-10 20:08:40 -0400

    FEAT: mudanças na logic.py salvando dados em json correto.

--- codes/logic.py
+++ codes/logic.py
@@ -1,3 +1,4 @@
+import json
 import os.path
 from google.oauth2.credentials import Credentials
 from google.auth.transport.requests import Request
@@ -6,6 +7,7 @@ from googleapiclient.discovery import build
 
 # Defina as permissões necessárias
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
+
 
 def main():
     creds = None
@@ -32,8 +34,8 @@ def main():
     service = build("sheets", "v4", credentials=creds)
 
     # ID da planilha e intervalos
-    spreadsheet_id = "1D8Lrx73soJV6BblRmsHVFVuDjnGifPf2QZlGsMKeI0E"
-    ranges = ["Respostas ao formulário 1!A:A", "Respostas ao formulário 1!C:C", "Respostas ao formulário 1!D:D", "Respostas ao formulário 1!P:P2"]
+    spreadsheet_id = "1Tk5U-uDoguGsmyMdIqFimDoz4W8opgsxtHbw8EQrvUw"
+    ranges = ["Respostas ao formulário 1!A:N"]
 
     # Chama a API Sheets para obter os valores dos intervalos especificados
     request = service.spreadsheets().values().batchGet(
@@ -45,16 +47,27 @@ def main():
     response = request.execute()
 
     # Processa as respostas para cada intervalo
+    lab_data_dict = {}
     for value_range in response["valueRanges"]:
-        range_name = value_range["range"]
         values = value_range.get("values", [])
 
         if not values:
-            print(f"No data found in range: {range_name}")
+            print(f"No data found in range")
         else:
+            # Itera sobre os valores para extrair os dados de cada laboratório
+            for row in range(1, len(values[0])):  # Começa a partir da segunda linha (a primeira linha contém os cabeçalhos)
+                lab_name = values[2][row]  # Nome do laboratório na coluna C
+                lab_data = {
+                    "nome": values[2][row],  # Nome do laboratório
+                    "N_maquinas": int(values[3][row]),  # Número de máquinas (convertido para inteiro)
+                    "softweres_instalados": values[6][row].split(", ")  # Softwares instalados
+                }
+                lab_data_dict[f"laboratorio_{row}"] = lab_data
 
-            for row in values:
-                print(row)
+    # Salva os dados em um arquivo JSON
+    with open("output.json", "w") as json_file:
+        json.dump(lab_data_dict, json_file, indent=4)
+
 
 if __name__ == "__main__":
     main()
