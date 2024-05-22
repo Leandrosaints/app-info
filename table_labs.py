9b5769e0a10b6fffe39137af9145547d42b82a4d
Leandrosaints <l210598l>
2024-05-22 16:50:57 -0400

    FEAT:subindo alteracao no table_labs.py, tabela para agendamentos de labs

--- table_labs.py
+++ table_labs.py
@@ -3,13 +3,14 @@ import pandas as pd
 
 # Criando um exemplo de dataframe com 7 linhas e 5 colunas (dias da semana)
 data = {
-    'Segunda': [25, 30, 35, 28, 40, 33, 27],
-    'Terça': [22, 27, 32, 25, 38, 31, 24],
-    'Quarta': [28, 33, 38, 31, 43, 36, 29],
-    'Quinta': [20, 25, 30, 23, 35, 28, 21],
-    'Sexta': [24, 29, 34, 27, 39, 32, 25]
+    'LAB': ['LAB AUTOMOTIVA', 'Lab 02', 'Lab 03', 'Lab 04', 'Lab 05', 'Lab 06', 'Lab 07'],
+    'Seg': ['prof: Leandro', 'prof: Maria', 'prof: Pedro', 'prof: Ana', 'prof: Lucas', 'prof: Fernanda', 'prof: João'],
+    'Ter': ['prof: Leandro', 'prof: Maria', 'prof: Pedro', 'prof: Ana', 'prof: Lucas', 'prof: Fernanda', 'prof: João'],
+    'Qua': ['prof: Leandro', 'prof: Maria', 'prof: Pedro', 'prof: Ana', 'prof: Lucas', 'prof: Fernanda', 'prof: João'],
+    'Qui': ['prof: Leandro', 'prof: Maria', 'prof: Pedro', 'prof: Ana', 'prof: Lucas', 'prof: Fernanda', 'prof: João'],
+    'Sex': ['prof: Leandro', 'prof: Maria', 'prof: Pedro', 'prof: Ana', 'prof: Lucas', 'prof: Fernanda', 'prof: João']
 }
-df = pd.DataFrame(data, index=['Lab 01', 'Lab 02', 'Lab 03', 'Lab 04', 'Lab 05', 'Lab 06', 'Lab 07'])
+df = pd.DataFrame(data)
 
 # Função para editar os valores da tabela
 def edit_cell(row, col, value):
@@ -17,7 +18,37 @@ def edit_cell(row, col, value):
     return df
 
 # Exibindo o dataframe como uma tabela no Streamlit e permitindo a edição
+st.set_page_config(layout="wide")  # Definir o layout do Streamlit como "wide"
 st.title("Agendamentos de Laboratorios")
+turno = st.selectbox("Selecione o turno:", ["Matutino", "Vespertino", "Noturno"])
+st.write(f"Você selecionou o turno: {turno}")
+
+# Adicionar estilo CSS personalizado
+st.markdown("""
+<style>
+    .dataframe {
+        font-size: 14px;
+        width: 100%;
+        overflow-x: auto;
+        white-space: nowrap;
+    }
+    .dataframe th, .dataframe td {
+        padding: 10px;
+        text-align: left;
+        border-bottom: 1px solid #ddd;
+    }
+    @media (max-width: 767px) {
+        .dataframe {
+            font-size: 12px;
+            padding: 5px;
+        }
+        .dataframe th, .dataframe td {
+            padding: 5px;
+        }
+    }
+</style>
+""", unsafe_allow_html=True)
+
 edited_df = st.data_editor(df, num_rows="dynamic", use_container_width=True)
 
 # Verificar se a tabela foi editada e atualizar o dataframe
