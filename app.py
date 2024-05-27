10df493f4483f89e5dc046e445f9d31932ef4f99
Leandrosaints <leo320487@gmail.com>
2024-05-27 12:38:10 -0400

    CHORE: Trablhando na funcao de escrita de planilhas. Obs: serios erros!

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
+import json
+
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import json
 import logging
+from codes.logica import write_sheet, read_sheet
 
 logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
 CORS(app)
-
 class LabData:
     def __init__(self, LAB, Seg, Ter, Qua, Qui, Sex):
         self.LAB = LAB
@@ -27,8 +28,9 @@ class LabData:
             "Sex": self.Sex
         }
 
-@app.route("/save_data", methods=["POST", "OPTIONS"])
-def save_data():
+@app.route("/write_data", methods=["POST"])
+def write_data():
+
     if request.method == "OPTIONS":
         return jsonify({})
 
@@ -36,15 +38,33 @@ def save_data():
         data = request.get_json()
         lab_data_list = [LabData(**item) for item in data]
         logging.debug("Received data: %s", lab_data_list)
-
-        with open('files/data.json', 'w') as f:
-            json.dump([item.to_dict() for item in lab_data_list], f, indent=4)
+        write_sheet([item.to_dict() for item in lab_data_list])
+        """with open('files/data.json', 'w') as f:
+            json.dump([item.to_dict() for item in lab_data_list], f, indent=4)"""
 
         logging.debug("Data saved successfully!")
         return jsonify({"message": "Data saved successfully!"})
     except Exception as e:
         logging.error("Error saving data: %s", e)
         return jsonify({"error": str(e)}), 500
+# Recebe os dados a serem escritos na planilha
+#data = request.get_json()
+
+#values = data["values"]
+
+# Chama a função de escrita
+#write_sheet(values)
+
+
+@app.route("/read_data", methods=["GET"])
+def read_data():
+    # Recebe os parâmetros para leitura da planilha
+    spreadsheet_id = request.args.get("spreadsheet_id")
+    range_name = request.args.get("range_name")
+
+    # Chama a função de leitura
+    values = read_sheet(spreadsheet_id, range_name)
+    return {"values": values}
 
 if __name__ == "__main__":
     app.run(debug=True)
