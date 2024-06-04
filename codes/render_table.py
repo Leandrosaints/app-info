6ae166d2bfec94fe2880dd2ef5b523c6186387dd
Leandrosaints <leo320487@gmail.com>
2024-06-04 10:16:48 -0400

    CHORE: criado uma nova versao do rende_table. nele esta todo codigo streamlit e hmtl. visualizaçao melhor porem ainda sem conseguir fazer a tabela atualizar sempre que um dado for inserido

--- codes/render_table.py
+++ codes/render_table.py
@@ -1,29 +1,26 @@
+import streamlit as st
 import pandas as pd
 import streamlit.components.v1 as components
 import requests
 
 def fetch_sheet_data(turno):
-    response = requests.get(f"https://app-info.onrender.com/read_data/{turno}")
+    response = requests.get(f"http://127.0.0.1:8080/read_data/{turno}")
     if response.status_code == 200:
-
         return response.json()["values"]
     else:
-        print("Desculpe, houve um erro no sistema, contacte os responsaveis")
+        st.error("Desculpe, houve um erro no sistema, contacte os responsáveis.")
         return []
 
 def draw_table(table_height, editable, save_url, turno):
     data = fetch_sheet_data(turno)
-
-    dias_da_semana = ["LAB","Seg", "Ter", "Qua", "Qui", "Sex"]
+    dias_da_semana = ["LAB", "Seg", "Ter", "Qua", "Qui", "Sex"]
     df = pd.DataFrame(data, columns=dias_da_semana)
     columns = df.columns
 
-    # Construir o cabeçalho da tabela
     thead1 = "<thead><tr><th style='background-color:#098aff;'scope='col'></th>"
     thead_temp = ["<th style='background-color:#098aff;'scope='col'>" + str(col) + "</th>" for col in columns]
     header = thead1 + "".join(thead_temp) + "</tr ></thead>"
 
-    # Construir o corpo da tabela
     rows = ["<tr><th scope='row'></th>" for _ in range(df.shape[0])]
     cells = []
     for i, row in enumerate(df.values.tolist()):
@@ -38,11 +35,6 @@ def draw_table(table_height, editable, save_url, turno):
         cells.append("".join(row_cells))
     body = "".join([rows[i] + cells[i] + "</tr>" for i in range(df.shape[0])])
 
-    # Montar a tabela completa com modal
-
-
-
-    # Montar a tabela completa com modal
     table_html = f"""
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
@@ -52,37 +44,29 @@ def draw_table(table_height, editable, save_url, turno):
             border: 1px solid #dee2e6;
             box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
         }}
-
-        .table th,
-        .table td {{
+        .table th, .table td {{
             border: 1px solid #dee2e6;
             padding: 0.75rem;
         }}
-
         .table thead th {{
             background-color: #007bff;
             color: #fff;
         }}
-
         .table tbody td {{
             background-color: #f8f9fa;
         }}
-
         .table tbody td.libre {{
             background-color: #1ef79399;
             color: #000;
         }}
-
         .table tbody td.professor {{
             background-color: #ffc107;
             color: #000;
         }}
-
         .table-responsive {{
             max-width: 100%;
             overflow-x: auto;
         }}
-
         .table {{
             width: 100%;
             max-width: 100%;
@@ -90,7 +74,6 @@ def draw_table(table_height, editable, save_url, turno):
             text-align: center;
         }}
     </style>
-
     <script>
         function openModal(row, col, column) {{
             var modal = new bootstrap.Modal(document.getElementById('editModal'));
@@ -100,7 +83,6 @@ def draw_table(table_height, editable, save_url, turno):
             document.getElementById('modalValue').value = document.querySelectorAll('tbody tr')[row].querySelectorAll('td')[col].textContent;
             modal.show();
         }}
-
         function confirmSave() {{
             var value = document.getElementById('modalValue').value;
             if (value.trim() === "" || value.trim().toLowerCase() === "livre") {{
@@ -110,7 +92,6 @@ def draw_table(table_height, editable, save_url, turno):
             var confirmModal = new bootstrap.Modal(document.getElementById('confirmModal'));
             confirmModal.show();
         }}
-
         function saveValue() {{
             var row = document.getElementById('modalRow').value;
             var col = document.getElementById('modalCol').value;
@@ -124,13 +105,11 @@ def draw_table(table_height, editable, save_url, turno):
             document.querySelectorAll('tbody tr')[row].querySelectorAll('td')[col].innerHTML = displayValue;
             updateDataFrame(row, column, displayValue);
         }}
-
         function updateDataFrame(row, column, value) {{
             var df = {df.to_json(orient='split')};
             df.data[row][df.columns.indexOf(column)] = value;
             saveDataToJSON(df);
         }}
-
         function saveDataToJSON(df) {{
            fetch('{save_url}', {{
                 method: 'POST',
@@ -161,7 +140,6 @@ def draw_table(table_height, editable, save_url, turno):
             }});
         }}
     </script>
-
     <div class="table-responsive">
         <table class="table table-bordered table-sm text-center">
             {header}
@@ -170,7 +148,6 @@ def draw_table(table_height, editable, save_url, turno):
             </tbody>
         </table>
     </div>
-
     <div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
@@ -184,7 +161,7 @@ def draw_table(table_height, editable, save_url, turno):
                     <input type="hidden" id="modalColumn">
                     <div class="mb-3">
                         <label for="modalValue" class="form-label">Digite apenas seu nome.</label>
-                        <label for="modalValue" class="form-label-one">Antes de salvar, verifique se o turno esta correto.</label>
+                        <label for="modalValue" class="form-label-one">Antes de salvar, verifique se o turno está correto.</label>
                         <input type="text" class="form-control" id="modalValue">
                     </div>
                 </div>
@@ -195,7 +172,6 @@ def draw_table(table_height, editable, save_url, turno):
             </div>
         </div>
     </div>
-
     <div class="modal fade" id="confirmModal" tabindex="-1" aria-labelledby="confirmModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
@@ -214,9 +190,11 @@ def draw_table(table_height, editable, save_url, turno):
         </div>
     </div>
     """
-
-
-
-
-
     return components.html(table_html, height=table_height, scrolling=True)
+
+st.title('Tabela Interativa')
+turno = st.selectbox("Selecione o turno:", ["Matutino", "Vespertino"])
+editable = st.checkbox("Permitir Edição")
+save_url = f"http://127.0.0.1:8080/write_data/{turno}"
+
+draw_table(600, editable, save_url, turno)
